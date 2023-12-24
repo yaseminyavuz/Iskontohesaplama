40dfe75e1574d307684ae23698a0a0b15884186b
Yasemin Yavuz <128582599+yaseminyavuz@users.noreply.github.com>
2023-12-24 14:54:13 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Iskontohesap
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bu program ürünün iskonto oranına göre yeni etiket fiyatını belirler.");
+            Console.Write("Ürünün satış fiyatını giriniz: ");
+            int urunfiyati=Convert.ToInt32(Console.ReadLine());
+            int iskonto = (urunfiyati * 20) / 100;
+            int etiketfiyati=urunfiyati-iskonto;
+            Console.WriteLine("Ürünün yeni etiket fiyatı: "+" " + etiketfiyati +" "+ "Türk Lirası'dır.");
+            Console.WriteLine("Bu ürün için bu ayki iskonto oranı % 20'dir.");
+            Console.ReadLine(); 
+
+           
+        }
+    }
+}
